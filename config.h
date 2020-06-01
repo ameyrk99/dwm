9ab9f294ba105da23e1b73546405a4b6dbe15d87
Amey Khairnar <ameykhairnar99@gmail.com>
2020-05-31 22:20:42 -0500

    Systray patch for polychromatic and others

--- config.h
+++ config.h
@@ -8,6 +8,10 @@
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int gappx     = 10;       /* gaps between windows */
 static const unsigned int snap      = 0;        /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int viewontag          = 1;        /* 0 means don't follow */
@@ -43,6 +47,7 @@ static const Rule rules[] = {
   { "Gimp",               NULL,           NULL,   1 << 4,       0,           -1 },
   { "discord",            NULL,           NULL,   1 << 5,       0,           -1 },
   { "Steam",              NULL,           NULL,   1 << 5,       0,           -1 },
+  { "retroarch",          NULL,           NULL,   1 << 5,       0,           -1 },
   { NULL,                 "libreoffice",  NULL,   1 << 6,       0,           -1 },
   { "Tor Browser",        NULL,           NULL,   1 << 8,       0,           -1 },
 }; // }}}1
@@ -74,7 +79,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-l", "5", "-m", dmenumon, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 // }}}1
@@ -164,8 +169,12 @@ static Key keys[] = {
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkStatusText,        ControlMask,    Button1,        spawn,          SHCMD("~/.dotfiles/scripts/wallpaper.sh ~/Pictures/Wallpapers/dwmwalls") },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkStatusText,        ControlMask,    Button2,        spawn,          SHCMD("~/.dotfiles/scripts/wallpaper.sh ~/Pictures/Wallpapers/dwmwalls") },
 }; // }}}1
 
